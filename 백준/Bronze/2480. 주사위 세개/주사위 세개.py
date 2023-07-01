a7bca5e5d20177565e1884d38ba995dc96877ccf
Today-ijeong <109265504+Today-ijeong@users.noreply.github.com>
2023-07-01 14:46:34 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,16 +1,2 @@
-a,b,c = map(int,input().split())
-
-if (a==b and a==c):
-    print(10000+a*1000)
-elif (a==b and a!=c):
-    print(1000+a*100)
-elif(a==c and a!=b):
-    print(1000+c*100)
-elif(b==c and a!=c):
-    print(1000+b*100)
-elif (a!= b and a!=c and a>b and a>c):
-        print(a*100)
-elif (a!= b and a!=c and b>a and b>c):
-        print(b*100)
-else:
-    print(c*100) 
+*_,a,b,c=sorted(input())
+print(['1'+b,c][a<b<c]+'000'[a<c:])
