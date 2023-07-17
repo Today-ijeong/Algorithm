33609fd7b4eff9dae5011c850ad966f46e731ad0
Today-ijeong <109265504+Today-ijeong@users.noreply.github.com>
2023-07-17 15:14:00 +0900

    [Bronze V] Title: A+B - 8, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,11 +1,11 @@
+T = int(input())
 c = []
 d = []
 e = []
-t = int(input())
-for i in range(0,t):
-    a,b= list(map(int,input().split()))
+for i in range(T):
+    a,b = list(map(int,input().split()))
     c.append(a+b)
     d.append(a)
     e.append(b)
-for j in range(1,t+1):
-    print("Case #%d:"%j,d[j-1],"+",e[j-1],"=",c[j-1])
+for j in range(T):
+    print("Case","#"+str(j+1)+":",d[j],"+",e[j],"=",c[j])
