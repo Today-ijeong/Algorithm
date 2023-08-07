9500a8d262b3935665922ce12faa5ff379b08936
Today-ijeong <109265504+Today-ijeong@users.noreply.github.com>
2023-08-07 23:21:09 +0900

    [Bronze II] Title: 트럭 주차, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.py
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.py
@@ -1,20 +1,7 @@
-a,b,c = map(int,input().split())
-time = [0]*100
-sum = 0
-
+cnt = [0]*100
+a,b,c = list(map(int,input().split()))
 for _ in range(3):
-    start,end = map(int,input().split())
+    start, end = map(int,input().split())
     for i in range(start,end):
-        time[i]+=1
-
-for j in time:
-    if j==0:
-        sum+=0
-    elif j==1:
-        sum+=a
-    elif j==2:
-        sum+=(b*2)
-    elif j==3:
-        sum+=(c*3)
-
-print(sum)
+        cnt[i]+=1
+print(cnt.count(1)*a +cnt.count(2)*b*2+cnt.count(3)*c*3)
