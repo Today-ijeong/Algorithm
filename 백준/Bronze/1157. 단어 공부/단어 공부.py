5dbeccbe46d3fc7bc34b318cc4c4024c7ef26050
Today-ijeong <109265504+Today-ijeong@users.noreply.github.com>
2023-08-11 01:18:26 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33212 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,9 @@
+cnt = []
+word = input().lower()
+word_list = list(set(word.lower()))
+for i in word_list:
+    cnt.append(word.count(i))
+if cnt.count(max(cnt))>=2:
+    print("?")
+else:
+    print(word_list[cnt.index(max(cnt))].upper())
